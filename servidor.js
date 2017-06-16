7273e6eb6ea3fda843a6c0bcbacdbf9992905a82
David Diaz <david.diaz@zweicom.com>
2017-06-16 17:49:43 -0400

    se agrega el servidor.js

---
+++ servidor.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var server = http.createServer();
+
+function control(req, resp) {
+	resp.writeHead(200, {'content-type': 'text/plain'});
+	resp.write('Hola, Mundo!');
+	resp.end();
+}
+
+server.on( 'request', control );
+server.listen(8090);
